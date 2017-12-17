ca87eb1c3a5e2051a64242a9091c2f15d4d5df15
Weidongdong <redone17@163.com>
2017-12-17 11:24:38 +0800

    Add files via upload
    run this file

---
+++ train.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+# python2
+
+'''
+mian .py for conv-rotor project
+0. initialize
+1. get data;
+2. make models;
+3. train;
+4. test;
+5. visualize;
+'''
+
+# initialize
+from __future__ import print_function, division 
+
+import torch
+import torch.nn as nn
+import torch.nn.function as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+import numpy as np
+
+import time
+import copy
+import os
+
+# get data
+import data_loader
+rsn = True
+data_arr = load_data(('dis_data.txt','rpm_data.txt'),rsn)
+print(np.shape(data_arr))
