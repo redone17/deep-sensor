96949f9f3bc89bd62123f049a36e2df4af656e00
Weidongdong <redone17@163.com>
2017-12-18 22:36:19 +0800

    Update wcdnn.py
    fix indent

--- models/wcdnn.py
+++ models/wcdnn.py
@@ -13,39 +13,37 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 class BasicBlock(nn.Module):
-	def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
-		super(BasicBlock, self).__init__()
-		self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
-		self.bn = nn.BatchNorm1d(out_channels)
-		self.pool = nn.MaxPool1d(2,stride=2)
-	
-	def forward(self, x):
-		x = self.conv(x)
-		x = self.bn(x)
-		x = self.pool(x)
-		x = F.relu(x)
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    super(BasicBlock, self).__init__()
+    self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+    self.bn = nn.BatchNorm1d(out_channels)
+    self.pool = nn.MaxPool1d(2,stride=2)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.pool(x)
+        x = F.relu(x)
 
 class WDCNN(nn.Module):
-	def __init__(self, n_class, use_feature=False):
-		super(WDCNN, self).__init__()
-		self.conv = nn.Sequential(
-			nn.BatchNorm1d(1)
-			BasicBlock( 1, 16, 64, 16, 24)
-			BasicBlock(16, 32)
-			BasicBlock(32, 64)
-			BasicBlock(64, 64)
-			BasicBlock(64, 64, padding=0)
-		)
-		self.n_features = 64*3
-		self.fc = nn.Linear(n_features, n_class)
+    def __init__(self, n_class, use_feature=False):
+        super(WDCNN, self).__init__()
+        self.conv = nn.Sequential(
+            nn.BatchNorm1d(1)
+            BasicBlock( 1, 16, 64, 16, 24)
+            BasicBlock(16, 32)
+            BasicBlock(32, 64)
+            BasicBlock(64, 64)
+            BasicBlock(64, 64, padding=0)
+        )
+        self.n_features = 64*3
+        self.fc = nn.Linear(n_features, n_class)
 
-	def forward(self, x):
-		features = self.conv(x)
-		activations = self.fc(features.view(-1, n_features))
-		if use_feature:
-			out = (activations, features)
-		else:
-			out = (activations, None)
-		return out
-
-
+    def forward(self, x):
+        features = self.conv(x)
+        activations = self.fc(features.view(-1, n_features))
+        if use_feature:
+            out = (activations, features)
+        else:
+            out = (activations, None)
+        return out
