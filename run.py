71e323a38971efb533c5be6e213185803ea4d245
redone17 <redone17@163.com>
2018-01-02 11:35:10 +0800

    add domain adaption

--- run.py
+++ run.py
@@ -21,17 +21,21 @@ import torch.utils.data as data_utils
 from models import *
 
 ## load data
-data_arr = data_loader.load_data('dis_data.txt')
+data_arr_01 = data_loader.load_data('fluc01/dis_data.txt')
+data_arr_03 = data_loader.load_data('fluc03/dis_data.txt')
 # data_arr = data_arr[:,:,:240] # add for Ince's model
 # amp, ang = data_loader.fft_arr(data_arr) # add for fft wdcnn
 # spectrogram = data_loader.stft_arr(data_arr) # add for stft-LeNet
 label_vec = data_loader.load_label('label_vec.txt')
-train_dict, test_dict = data_loader.split_arr(data_arr, label_vec)
-trainset = data_loader.arr_to_dataset(train_dict['data'], train_dict['label'])
-testset = data_loader.arr_to_dataset(test_dict['data'], test_dict['label'])
+train_dict_01, test_dict_01 = data_loader.split_arr(data_arr_01, label_vec)
+train_dict_03, test_dict_03 = data_loader.split_arr(data_arr_03, label_vec)
+trainset_01 = data_loader.arr_to_dataset(train_dict_01['data'], train_dict_01['label'])
+testset_01 = data_loader.arr_to_dataset(test_dict_01['data'], test_dict_01['label'])
+trainset_03 = data_loader.arr_to_dataset(train_dict_03['data'], train_dict_03['label'])
+testset_03 = data_loader.arr_to_dataset(test_dict_03['data'], test_dict_03['label'])
 
-train_loader = data_utils.DataLoader(dataset = trainset, batch_size = 200, shuffle = True, num_workers = 2)
-test_loader = data_utils.DataLoader(dataset = testset, batch_size = 200, shuffle = True, num_workers = 2)
+train_loader = data_utils.DataLoader(dataset = trainset_01, batch_size = 200, shuffle = True, num_workers = 2)
+test_loader = data_utils.DataLoader(dataset = testset_03, batch_size = 200, shuffle = True, num_workers = 2)
 print('Number of training samples: {}'.format(len(train_loader.dataset)))
 print('Number of testing samples: {}'.format(len(test_loader.dataset)))
 print( )
@@ -41,9 +45,9 @@ model = wdcnn.Net(1, 4)
 
 ## train
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), weight_decay=0.001 )
+optimizer = optim.Adam(model.parameters(), weight_decay=0.0001)
 best_model, loss_curve = iter_utils.train(model, train_loader, criterion, optimizer,
-    init_lr=0.01, decay_epoch=5, n_epoch=3)
+    init_lr=0.0001, decay_epoch=5, n_epoch=20)
 
 # test
 test_accuracy = iter_utils.test(best_model, test_loader)
@@ -51,3 +55,4 @@ print('Test accuracy: {:.4f}%'.format(100*test_accuracy))
 
 ## visualization
 # TODO
+
